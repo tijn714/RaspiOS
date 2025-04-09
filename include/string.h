06c3582eeca487bc929435985052797b9e1c1ba2
Tijn Rodrigo <71492011+tijn714@users.noreply.github.com>
2025-04-09 22:23:00 +0200

    string driver

---
+++ include/string.h
@@ -0,0 +1,43 @@
+#ifndef STRING_H
+#define STRING_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+typedef unsigned int size_t;
+#define NULL ((void*)0UL)
+
+// strlen
+size_t strlen(const char* str);
+
+// itoa
+char* itoa(int value, char* str, int base);
+
+//memset
+void* memset(void* ptr, int value, size_t num);
+
+//memcpy
+void* memcpy(void* dest, const void* src, size_t num);
+
+// strstr
+char* strstr(const char* haystack, const char* needle);
+
+// strncmp
+int strncmp(const char* str1, const char* str2, size_t num);
+
+// strcmp
+int strcmp(const char* str1, const char* str2);
+
+// is functions
+bool isEqual(const uint8_t* a, const uint8_t* b, size_t size);
+bool isint(char c);
+
+int isspace(char c);
+
+int isalpha(char c);
+char upper(char c);
+char lower(char c);
+
+void clear_buffer(char* buffer);
+
+#endif // STRING_H 
